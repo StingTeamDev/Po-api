86bceca0db60a6ccb3a51990b399d8ba351a0020
StingTeamDev <97679574+StingTeamDev@users.noreply.github.com>
2022-08-02 21:46:21 +0200

    Update j.js

--- j.js
+++ j.js
@@ -1,6 +1,8 @@
-const JSap = async function get() {
+const JSapi = async function get() {
   /* API POSTS */
-  const response = await fetch("https://almaghrebsport.com/wp-json/wp/v2/posts");
+  const response = await fetch(
+    "https://almaghrebsport.com/wp-json/wp/v2/posts"
+  );
   const data = await response.json();
 
   const responsemedia = await fetch(
@@ -12,48 +14,40 @@ const JSap = async function get() {
     .getElementsByTagName("head")[0]
     .insertAdjacentHTML(
       "beforeend",
-      '<link rel="stylesheet" href="//cdn.statically.io/gh/StingTeamDev/Po-api/main/cdn.css" />'
+      '<link rel="stylesheet" href="https://cdn.statically.io/gh/StingTeamDev/Po-api/main/c.css"/>'
     );
 
   document.querySelector("#PAutoSting1 img").src = media[0].guid.rendered;
   document.querySelector("#PAutoSting1 img").alt = data[0].title.rendered;
-  document.querySelector("#PAutoSting1 a").innerHTML =
-    data[0].title.rendered;
+  document.querySelector("#PAutoSting1 a").innerHTML = data[0].title.rendered;
 
   document.querySelector("#PAutoSting2 img").src = media[1].guid.rendered;
   document.querySelector("#PAutoSting2 img").alt = data[1].title.rendered;
-  document.querySelector("#PAutoSting2 a").innerHTML =
-    data[1].title.rendered;
+  document.querySelector("#PAutoSting2 a").innerHTML = data[1].title.rendered;
 
   document.querySelector("#PAutoSting3 img").src = media[2].guid.rendered;
   document.querySelector("#PAutoSting3 img").alt = data[2].title.rendered;
-  document.querySelector("#PAutoSting3 a").innerHTML =
-    data[2].title.rendered;
+  document.querySelector("#PAutoSting3 a").innerHTML = data[2].title.rendered;
 
   document.querySelector("#PAutoSting4 img").src = media[3].guid.rendered;
   document.querySelector("#PAutoSting4 img").alt = data[3].title.rendered;
-  document.querySelector("#PAutoSting4 a").innerHTML =
-    data[3].title.rendered;
+  document.querySelector("#PAutoSting4 a").innerHTML = data[3].title.rendered;
 
   document.querySelector("#PAutoSting5 img").src = media[4].guid.rendered;
   document.querySelector("#PAutoSting5 img").alt = data[4].title.rendered;
-  document.querySelector("#PAutoSting5 a").innerHTML =
-    data[4].title.rendered;
+  document.querySelector("#PAutoSting5 a").innerHTML = data[4].title.rendered;
 
   document.querySelector("#PAutoSting6 img").src = media[5].guid.rendered;
   document.querySelector("#PAutoSting6 img").alt = data[5].title.rendered;
-  document.querySelector("#PAutoSting6 a").innerHTML =
-    data[5].title.rendered;
+  document.querySelector("#PAutoSting6 a").innerHTML = data[5].title.rendered;
 
   document.querySelector("#PAutoSting7 img").src = media[6].guid.rendered;
   document.querySelector("#PAutoSting7 img").alt = data[6].title.rendered;
-  document.querySelector("#PAutoSting7 a").innerHTML =
-    data[6].title.rendered;
+  document.querySelector("#PAutoSting7 a").innerHTML = data[6].title.rendered;
 
   document.querySelector("#PAutoSting8 img").src = media[7].guid.rendered;
   document.querySelector("#PAutoSting8 img").alt = data[7].title.rendered;
-  document.querySelector("#PAutoSting8 a").innerHTML =
-    data[7].title.rendered;
+  document.querySelector("#PAutoSting8 a").innerHTML = data[7].title.rendered;
 
   /* API POSTS */
 
@@ -69,8 +63,11 @@ const JSap = async function get() {
 
   closepostapi.addEventListener("click", () => {
     Content.classList.remove("actived");
-    Content.style.display = "none";
   });
+
+  const Sting =
+    "Posts auto api ~> Version : 1.0 BETA - Powered by - Sting Web     Facebook Page Call us --- https://www.facebook.com/stingweb.eg";
+  console.log(Sting);
 
   const Poo1 = document.querySelector(".Poo1");
   const Poo2 = document.querySelector(".Poo2");
@@ -82,52 +79,44 @@ const JSap = async function get() {
   const Poo8 = document.querySelector(".Poo8");
 
   Poo1.addEventListener("click", () => {
-    document.querySelector(".contentv1 h1").innerHTML = data[0].title.rendered;
     document.querySelector(".contentv1 img").src = media[0].guid.rendered;
     document.querySelector(".contentv1 p").innerHTML = data[0].content.rendered;
   });
 
   Poo2.addEventListener("click", () => {
-    document.querySelector(".contentv1 h1").innerHTML = data[1].title.rendered;
     document.querySelector(".contentv1 img").src = media[1].guid.rendered;
     document.querySelector(".contentv1 p").innerHTML = data[1].content.rendered;
   });
 
   Poo3.addEventListener("click", () => {
-    document.querySelector(".contentv1 h1").innerHTML = data[2].title.rendered;
     document.querySelector(".contentv1 img").src = media[2].guid.rendered;
     document.querySelector(".contentv1 p").innerHTML = data[2].content.rendered;
   });
 
   Poo4.addEventListener("click", () => {
-    document.querySelector(".contentv1 h1").innerHTML = data[3].title.rendered;
     document.querySelector(".contentv1 img").src = media[3].guid.rendered;
     document.querySelector(".contentv1 p").innerHTML = data[3].content.rendered;
   });
 
   Poo5.addEventListener("click", () => {
-    document.querySelector(".contentv1 h1").innerHTML = data[4].title.rendered;
     document.querySelector(".contentv1 img").src = media[4].guid.rendered;
     document.querySelector(".contentv1 p").innerHTML = data[4].content.rendered;
   });
 
   Poo6.addEventListener("click", () => {
-    document.querySelector(".contentv1 h1").innerHTML = data[5].title.rendered;
     document.querySelector(".contentv1 img").src = media[5].guid.rendered;
     document.querySelector(".contentv1 p").innerHTML = data[5].content.rendered;
   });
 
   Poo7.addEventListener("click", () => {
-    document.querySelector(".contentv1 h1").innerHTML = data[6].title.rendered;
     document.querySelector(".contentv1 img").src = media[6].guid.rendered;
     document.querySelector(".contentv1 p").innerHTML = data[6].content.rendered;
   });
 
   Poo8.addEventListener("click", () => {
-    document.querySelector(".contentv1 h1").innerHTML = data[7].title.rendered;
     document.querySelector(".contentv1 img").src = media[7].guid.rendered;
     document.querySelector(".contentv1 p").innerHTML = data[7].content.rendered;
   });
 };
 
-JSap();
+JSapi();
